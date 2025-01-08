33139efc97294b27eaba845cd0e7382d2204d43e
erick16-max <erickgege16@gmail.com>
2025-01-08 10:22:33 +0300

    social media handles

--- components/footer/FooterGrid.js
+++ components/footer/FooterGrid.js
@@ -1,104 +1,173 @@
-import { Grid, Stack, Typography, Box, IconButton } from '@mui/material'
-import Image from 'next/image'
-import React, { useContext } from 'react'
-import LogoImage from "../../public/logo.png"
+import {
+  Grid,
+  Stack,
+  Typography,
+  Box,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
+import Image from "next/image";
+import React, { useContext } from "react";
+import LogoImage from "../../public/logo.png";
 import { MdEmail } from "react-icons/md";
-import Link from 'next/link';
+import Link from "next/link";
 import { IoCall } from "react-icons/io5";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { FaInstagramSquare } from "react-icons/fa";
 import { AiFillTikTok } from "react-icons/ai";
 import { FaSquareXTwitter, FaHeart } from "react-icons/fa6";
-import { menuItems } from '../header/MenuList';
-import AppContext from '@/context/AppContext';
-import { usePathname } from 'next/navigation';
-import GetQuoteButton from './GetQuoteButton';
-
+import { menuItems } from "../header/MenuList";
+import AppContext from "@/context/AppContext";
+import { usePathname } from "next/navigation";
+import GetQuoteButton from "./GetQuoteButton";
+import InstagramLogo from "../../public/instagram.png";
+import FacebookLogo from "../../public/facebook.png";
+import TwitterLogo from "../../public/twitter.png";
 
 export default function FooterGrid() {
-    const {isUser} = useContext(AppContext)
-    const pathName = usePathname()
+  const { isUser } = useContext(AppContext);
+  const pathName = usePathname();
   return (
     <Grid container rowSpacing={5} columnSpacing={2} p={4}>
-        <Grid item lg={4} md={4} sm={12} xs={12}>
-            <Stack width={'100%'} gap={1}>
-                <Image 
-                    src={LogoImage}
-                    alt='Atlatanta Cheap Tree cutting solution'
-                    width={150}
-                />
-                <Stack direction={'row'} color={'#dedede'} alignItems={'center'} gap={'4px'}>
-                    {/* <MdEmail fontSize={20}/>  */}
-                    <Typography variant='body2' color={'#dedede'} fontWeight={500}>Email:</Typography>
-                    <Link href={'mailto:atlantacheaptreesolutions@gmail.com'} style={{color: 'inherit'}}>
-                        <Typography variant='body2' color={'#eeeeee'} fontWeight={500}>atlantacheaptreesolutions@gmail.com</Typography>
-                    </Link>
-                </Stack>
-                <Stack direction={'row'} color={'#dedede'} alignItems={'center'} gap={'4px'}>
-                    {/* <IoCall fontSize={20}/>  */}
-                    <Typography variant='body2' color={'#dedede'} fontWeight={500}>Call:</Typography>
-                    <Link href={"tel:770-589-4000"} style={{color: 'inherit'}}>
-                        <Typography variant='body2' color={'#eeeeee'} fontWeight={500}>770 589 4000</Typography>
-                    </Link>
-                </Stack>
-                <Box
-         display={"flex"}
-         alignItems={"center"}
-         gap={2}
-      >
+      <Grid item lg={4} md={4} sm={12} xs={12}>
+        <Stack width={"100%"} gap={1}>
+          <Image
+            src={LogoImage}
+            alt="Atlatanta Cheap Tree cutting solution"
+            width={150}
+          />
+          <Stack
+            direction={"row"}
+            color={"#dedede"}
+            alignItems={"center"}
+            gap={"4px"}
+          >
+            {/* <MdEmail fontSize={20}/>  */}
+            <Typography variant="body2" color={"#dedede"} fontWeight={500}>
+              Email:
+            </Typography>
+            <Link
+              href={"mailto:atlantacheaptreesolutions@gmail.com"}
+              style={{ color: "inherit" }}
+            >
+              <Typography variant="body2" color={"#eeeeee"} fontWeight={500}>
+                atlantacheaptreesolutions@gmail.com
+              </Typography>
+            </Link>
+          </Stack>
+          <Stack
+            direction={"row"}
+            color={"#dedede"}
+            alignItems={"center"}
+            gap={"4px"}
+          >
+            {/* <IoCall fontSize={20}/>  */}
+            <Typography variant="body2" color={"#dedede"} fontWeight={500}>
+              Call:
+            </Typography>
+            <Link href={"tel:770-589-4000"} style={{ color: "inherit" }}>
+              <Typography variant="body2" color={"#eeeeee"} fontWeight={500}>
+                770 589 4000
+              </Typography>
+            </Link>
+          </Stack>
+          <Box display={"flex"} alignItems={"center"} gap={2}>
+            <Tooltip title="facebook" arrow>
+              <IconButton
+                  sx={{
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                    '&:hover': {
+                        backgroundColor: 'rgba(255, 255, 255, 0.5)'
+                    }
+                }}
+              >
+                <Image src={FacebookLogo} width={20} height={20} alt="fb" />
+              </IconButton>
+            </Tooltip>
 
-          <IconButton>
-              <FaSquareFacebook style={{fontSize: 20, color: '#eeeeee'}}/>
-          </IconButton>
-          <IconButton>
-              <FaInstagramSquare style={{fontSize: 20, color: '#eeeeee'}}/>
-          </IconButton>
-          <IconButton>
-              <FaSquareXTwitter style={{fontSize: 20, color: '#eeeeee'}}/>
-          </IconButton>
-      </Box>
-            </Stack>
-        </Grid>
-       
-        <Grid item lg={4}  md={4} sm={12} xs={12}>
-            <Stack gap={1}>
-                <Typography variant='body1' fontWeight={600} color={'#eeeeee'} gutterBottom>Useful Links</Typography>
+            <Tooltip title="instagram" arrow>
+              <IconButton
+                  sx={{
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                    '&:hover': {
+                        backgroundColor: 'rgba(255, 255, 255, 0.5)'
+                    }
+                }}
+              >
+                <Image src={InstagramLogo} width={20} height={20} alt="ig" />
+              </IconButton>
+            </Tooltip>
 
-                {
-                    menuItems.map(menu => {
-                        return(
-                            <Link 
-                                key={menu.id}
-                                href={menu.path} 
-                                color='inherit' 
-                                style={{
-                                    textDecoration: 'none',
-                                    display: menu.name === "Dashboard" && !isUser ? 'none' : ''
-                                    }}
-                            >
-                                <Typography variant='body1' fontWeight={500} color={'#dedede'}>
-                                    {menu.name}
-                                </Typography>
-                            </Link>
-                        )
-                    })
-                }
-                
-            </Stack>
-        </Grid>
-        <Grid item lg={4} md={4} sm={12} xs={12}>
-            <Stack>
-                <Typography variant='h6' fontWeight={600} color={'#eeeeee'} gutterBottom>
-                    Get In Touch
+            <Tooltip title="X formely twitter" arrow>
+              <IconButton
+                sx={{
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                    '&:hover': {
+                        backgroundColor: 'rgba(255, 255, 255, 0.5)'
+                    }
+                }}
+              >
+                <Image src={TwitterLogo} width={20} height={20} alt="X" />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Stack>
+      </Grid>
+
+      <Grid item lg={4} md={4} sm={12} xs={12}>
+        <Stack gap={1}>
+          <Typography
+            variant="body1"
+            fontWeight={600}
+            color={"#eeeeee"}
+            gutterBottom
+          >
+            Useful Links
+          </Typography>
+
+          {menuItems.map((menu) => {
+            return (
+              <Link
+                key={menu.id}
+                href={menu.path}
+                color="inherit"
+                style={{
+                  textDecoration: "none",
+                  display: menu.name === "Dashboard" && !isUser ? "none" : "",
+                }}
+              >
+                <Typography variant="body1" fontWeight={500} color={"#dedede"}>
+                  {menu.name}
                 </Typography>
-                <Typography variant='body1' fontWeight={400} color={'#dedede'} gutterBottom>
-                Thank you for considering Atlanta Cheap Tree Solutions for your tree care and 
-                yard cleanup needs. Contact us today to schedule a consultation or to learn more
-                 about how we can help you maintain a beautiful and safe outdoor environment.
-                </Typography>
-                <GetQuoteButton />
-            </Stack>
-        </Grid>
+              </Link>
+            );
+          })}
+        </Stack>
+      </Grid>
+      <Grid item lg={4} md={4} sm={12} xs={12}>
+        <Stack>
+          <Typography
+            variant="h6"
+            fontWeight={600}
+            color={"#eeeeee"}
+            gutterBottom
+          >
+            Get In Touch
+          </Typography>
+          <Typography
+            variant="body1"
+            fontWeight={400}
+            color={"#dedede"}
+            gutterBottom
+          >
+            Thank you for considering Atlanta Cheap Tree Solutions for your tree
+            care and yard cleanup needs. Contact us today to schedule a
+            consultation or to learn more about how we can help you maintain a
+            beautiful and safe outdoor environment.
+          </Typography>
+          <GetQuoteButton />
+        </Stack>
+      </Grid>
     </Grid>
-  )
+  );
 }
--- components/header/TopBar.js
+++ components/header/TopBar.js
@@ -1,107 +1,91 @@
-import { Box, Stack, Typography, IconButton, Tooltip, useTheme, useMediaQuery} from '@mui/material'
-import Link from 'next/link'
-import React from 'react'
-import { FaFacebookSquare } from "react-icons/fa";
-import { FaSquareInstagram } from "react-icons/fa6";
-import { BsTwitterX } from "react-icons/bs";
-import { FaSquareXTwitter } from "react-icons/fa6";
-
+import {
+  Box,
+  Stack,
+  Typography,
+  IconButton,
+  Tooltip,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import Link from "next/link";
+import React from "react";
+import InstagramLogo from "../../public/instagram.png";
+import FacebookLogo from "../../public/facebook.png";
+import TwitterLogo from "../../public/twitter.png";
+import Image from "next/image";
 
 export default function TopBar() {
-    const theme = useTheme();
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-    const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
     <Box
-        width={'100%'}
-        sx={{
-            borderBottom: '1px solid #e0e0e0',
-            boxShadow: 0,
-        }}
+      width={"100%"}
+      sx={{
+        borderBottom: "1px solid #e0e0e0",
+        boxShadow: 0,
+      }}
     >
-     <Box
-        width={'100%'}
+      <Box
+        width={"100%"}
         height={"40px"}
-        display={'flex'}
-        justifyContent={'space-between'}
-        alignItems={'center'}
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
         px={isSmallScreen ? 1 : 3}
         gap={1}
-        
-    >
-            <Stack direction={'row'} gap={1} display={isSmallScreen ? "none" : 'flex'}>
-               <Tooltip
-                title="facebook"
-                arrow
-               >
-                <IconButton>
-                        <FaFacebookSquare
-                            style={{
-                                fontSize: 20,
-                                color: '#424242'
-                            }}
-                        />
-                    </IconButton>
-               </Tooltip>
-               <Tooltip
-                title="instagram"
-                arrow
-               >
-                <IconButton>
-                        <FaSquareInstagram
-                            style={{
-                                fontSize: 20,
-                                color: '#424242'
-                            }}
-                        />
-                    </IconButton>
-               </Tooltip>
+      >
+        <Stack
+          direction={"row"}
+          gap={1}
+          display={isSmallScreen ? "none" : "flex"}
+        >
+          <Tooltip title="facebook" arrow>
+            <IconButton>
+              <Image src={FacebookLogo} width={20} height={20} alt="fb" />
+            </IconButton>
+          </Tooltip>
 
-               <Tooltip
-                title="X(formely twitter)"
-                arrow
-               >
-                <IconButton>
-                        <FaSquareXTwitter
-                            style={{
-                                fontSize: 20,
-                                color: '#424242'
-                            }}
-                        />
-                    </IconButton>
-               </Tooltip>
-              
-    
-            </Stack>
-        
-            <Stack
-                direction={'row'}
-                gap={1}
+          <Tooltip title="instagram" arrow>
+            <IconButton>
+              <Image src={InstagramLogo} width={20} height={20} alt="ig" />
+            </IconButton>
+          </Tooltip>
+         
+
+          <Tooltip title="X formely twitter" arrow>
+            <IconButton>
+              <Image src={TwitterLogo} width={20} height={20} alt="X" />
+            </IconButton>
+          </Tooltip>
+        </Stack>
+
+        <Stack direction={"row"} gap={1}>
+          <Typography variant="body2" className="interFont">
+            Call us for a qoute:
+          </Typography>
+          <Link
+            href="tel:770-589-4000"
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <Typography
+              variant="body2"
+              className="interFont"
+              color={"text.primary"}
+              sx={{
+                textDecoration: "underline",
+                fontWeight: 600,
+              }}
             >
-                <Typography
-                    variant='body2'
-                    className='interFont'
-                >Call us for a qoute:</Typography>
-                <Link 
-                    href="tel:770-589-4000"
-                    style={{
-                        textDecoration: 'none'
-                    }}
-                >
-                
-                    <Typography
-                    variant='body2'
-                    className='interFont'
-                    color={'text.primary'}
-                    sx={{
-                        textDecoration: 'underline',
-                        fontWeight: 600
-                    }}
-                > (770) 589-4000</Typography>
-                </Link>
-            </Stack>
+              {" "}
+              (770) 589-4000
+            </Typography>
+          </Link>
+        </Stack>
+      </Box>
     </Box>
-    </Box>
-  )
+  );
 }
