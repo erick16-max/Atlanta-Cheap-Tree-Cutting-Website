757958bd73dc87c5a28c8833b4a95b0e5ecc7c88
erick16-max <erickgege16@gmail.com>
2025-01-15 18:37:37 +0300

    posted a booking on aouthenticated users

--- app/booking/page.js
+++ app/booking/page.js
@@ -45,7 +45,7 @@ export default function page() {
       <Stack mt={"80px"} py={2} px={isSmallScreen ? 1 : 3}>
         <Box px={isSmallScreen ? 1 : 5} width={"100%"}>
             <CustomBreadcrumb current={'Booking'}/>
-            <BoorkingSteps />
+            {/* <BoorkingSteps /> */}
         </Box>
       </Stack>
       <Footer />
