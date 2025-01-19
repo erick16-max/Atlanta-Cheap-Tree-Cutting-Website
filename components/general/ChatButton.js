3030ae558025ae59f03123598b300510a8b2e09c
erick16-max <erickgege16@gmail.com>
2025-01-19 09:43:44 +0300

    chat with us button

--- components/general/ChatButton.js
+++ components/general/ChatButton.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Fab from '@mui/material/Fab';
-import { Tooltip, Typography } from '@mui/material';
+import React from "react";
+import Fab from "@mui/material/Fab";
+import { Tooltip, Typography } from "@mui/material";
 import { IoChatboxEllipsesOutline } from "react-icons/io5";
 import { TbMessage2 } from "react-icons/tb";
+import Link from "next/link";
 
 export default function ChatFloatingButton() {
   return (
@@ -11,21 +12,25 @@ export default function ChatFloatingButton() {
         color="primary"
         aria-label="chat"
         sx={{
-          position: 'fixed',
+          position: "fixed",
           bottom: 16,
           right: 16,
           zIndex: 1000,
-          width: 120, // Adjust for square shape
-          height: 48, // Adjust for square shape
-          borderRadius: 4, // Makes it square instead of round
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#1b5e20'
+          width: 120,
+          height: 48,
+          borderRadius: 4,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#1b5e20",
         }}
+        LinkComponent={Link}
+        href="https://wa.me/17705894000?text=Hello%20there!%20I%20would%20like%20to%20inquire%20about%20your%20tree%20services."
+        target="_blank"
+        rel="noopener noreferrer"
       >
         <TbMessage2 style={{ marginRight: 8 }} size={20} />
-        <Typography variant="button" sx={{ textTransform: 'None' }}>
+        <Typography variant="button" sx={{ textTransform: "None" }}>
           Chat Us
         </Typography>
       </Fab>
