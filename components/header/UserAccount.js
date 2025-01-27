516f1c882556ea4929851f1b9f5fbeed6ca127a0
erick16-max <erickgege16@gmail.com>
2025-01-27 12:33:24 +0300

    changed contact us button and added book us btn

--- components/header/UserAccount.js
+++ components/header/UserAccount.js
@@ -24,6 +24,7 @@ export default function UserAccount({isTablet, setOpenDrawer, user}) {
         </Stack>
       ):(
         <Stack direction={"row"} gap={1} alignItems={'center'}>
+
         <Button
           color="secondary"
           sx={{
