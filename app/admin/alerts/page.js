058f2e87d91dfa9d1137cd63f00e8761aff41729
erick16-max <erickgege16@gmail.com>
2025-01-28 18:38:52 +0300

    submit message/feedback

--- app/admin/alerts/page.js
+++ app/admin/alerts/page.js
@@ -1,14 +1,8 @@
-"use client"
 import React from 'react'
 import AdminPageLayout from '../AdminPageLayout'
-import { useRouter } from 'next/navigation'
-import PageLoader from '@/components/general/PageLoader'
 
 export default function page() {
-  const router = useRouter()
-  router.push('/')
-  return <PageLoader />
-  // return (
-  //   <AdminPageLayout>admin alerts page</AdminPageLayout>
-  // )
+  return (
+    <AdminPageLayout>admin alerts page</AdminPageLayout>
+  )
 }
